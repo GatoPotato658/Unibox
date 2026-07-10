78b9f8ac4fdbe34bcbada38d82a2f3dd9e58ed83
vel <dobryfraudzichaxd@proton.me>
2026-07-10 16:08:09 +0200

    spaceing

--- Amalgam/src/Features/ImGui/Menu/Menu.cpp
+++ Amalgam/src/Features/ImGui/Menu/Menu.cpp
@@ -2338,7 +2338,7 @@ void CMenu::MenuAnticheat(int iTab)
 
 			if (vCheaters.empty())
 			{
-				SetCursorPos({ H::Draw.Scale(15), H::Draw.Scale(70) });
+				SetCursorPos({ H::Draw.Scale(15), H::Draw.Scale(60) });
 				FText(cheater_search.empty() ? "Nothings here..." : "No matching cheaters.");
 				DebugDummy({ 0, H::Draw.Scale(8) });
 			}
@@ -2397,7 +2397,7 @@ void CMenu::MenuAnticheat(int iTab)
 						const float flPadding = H::Draw.Scale(8);
 						const float flTextOffset = flPadding * 2 + flAvatarSize;
 
-						ImVec2 vOriginalPos = { !x ? GetStyle().WindowPadding.x : GetWindowWidth() / 2 + GetStyle().WindowPadding.x / 2, H::Draw.Scale(70 + 64 * y) };
+						ImVec2 vOriginalPos = { !x ? GetStyle().WindowPadding.x : GetWindowWidth() / 2 + GetStyle().WindowPadding.x / 2, H::Draw.Scale(64 + 64 * y) };
 						float flWidth = GetWindowWidth() / 2 - GetStyle().WindowPadding.x * 1.5f;
 						float flHeight = H::Draw.Scale(56);
 						ImVec2 vDrawPos = GetDrawPos() + vOriginalPos;
@@ -2570,7 +2570,7 @@ void CMenu::MenuAnticheat(int iTab)
 					drawCheater(vCheaters[i], x, y);
 					if (!x) iBlu++; else iRed++;
 				}
-				SetCursorPos({ 0, H::Draw.Scale(71 + 64 * std::max(iBlu, iRed)) });
+				SetCursorPos({ 0, H::Draw.Scale(65 + 64 * std::max(iBlu, iRed)) });
 				DebugDummy({ 0, H::Draw.Scale(8) });
 			}
 		}
@@ -3417,7 +3417,7 @@ void CMenu::MenuLogs(int iTab)
 
 			if (vMarkedPlayers.empty())
 			{
-				SetCursorPos({ H::Draw.Scale(15), H::Draw.Scale(70) });
+				SetCursorPos({ H::Draw.Scale(15), H::Draw.Scale(60) });
 				FText(marked_player_search.empty() ? "Nothings here..." : "No matching marked players.");
 				DebugDummy({ 0, H::Draw.Scale(8) });
 			}
@@ -3439,7 +3439,7 @@ void CMenu::MenuLogs(int iTab)
 					const float flPadding = H::Draw.Scale(8);
 					const float flTextOffset = flPadding * 2 + flAvatarSize;
 
-					ImVec2 vOriginalPos = { !x ? GetStyle().WindowPadding.x : GetWindowWidth() / 2 + GetStyle().WindowPadding.x / 2, H::Draw.Scale(70 + 64 * y) };
+					ImVec2 vOriginalPos = { !x ? GetStyle().WindowPadding.x : GetWindowWidth() / 2 + GetStyle().WindowPadding.x / 2, H::Draw.Scale(64 + 64 * y) };
 					float flWidth = GetWindowWidth() / 2 - GetStyle().WindowPadding.x * 1.5f;
 					float flHeight = H::Draw.Scale(56);
 					ImVec2 vDrawPos = GetDrawPos() + vOriginalPos;
@@ -3618,7 +3618,7 @@ void CMenu::MenuLogs(int iTab)
 					else
 						iRed++;
 				}
-				SetCursorPos({ 0, H::Draw.Scale(71 + 64 * std::max(iBlu, iRed)) });
+				SetCursorPos({ 0, H::Draw.Scale(65 + 64 * std::max(iBlu, iRed)) });
 				DebugDummy({ 0, H::Draw.Scale(8) });
 			}
 		} EndSection();
